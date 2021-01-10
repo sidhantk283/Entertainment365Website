a5fc48e4e9c9eaa0ea8f04aa3da113d5945e39a2
sidhantk283 <sidhantk283@gmail.com>
2021-01-10 08:27:00 +0530

    Submitted HTML CSS

--- main.js
+++ main.js
@@ -1,90 +1,27 @@
-const slideImage = document.querySelectorAll(".slide-image");
-const slidesContainer = document.querySelector(".slides-container");
-const nextBtn = document.querySelector(".next-btn");
-const prevBtn = document.querySelector(".prev-btn");
-const navigationDots = document.querySelector(".navigation-dots");
 
-let numberOfImages = slideImage.length;
-let slideWidth = slideImage[0].clientWidth;
-let currentSlide = 0;
+var slideIndex = 1;
+showDivs(slideIndex);
 
-// Set up the slider
-
-function init() {
-  slideImage.forEach((img, i) => {
-    img.style.left = i * 100 + "%";
-  });
-
-  slideImage[0].classList.add("active");
-
-  createNavigationDots();
+function plusDivs(n) {
+  showDivs(slideIndex += n);
 }
 
-init();
-
-// Create navigation dots
-
-function createNavigationDots() {
-  for (let i = 0; i < numberOfImages; i++) {
-    const dot = document.createElement("div");
-    dot.classList.add("single-dot");
-    navigationDots.appendChild(dot);
-
-    dot.addEventListener("click", () => {
-      goToSlide(i);
-    });
-  }
-
-  navigationDots.children[0].classList.add("active");
+function currentDiv(n) {
+  showDivs(slideIndex = n);
 }
 
-// Next Button
-
-nextBtn.addEventListener("click", () => {
-  if (currentSlide >= numberOfImages - 1) {
-    goToSlide(0);
-    return;
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
   }
-
-  currentSlide++;
-  goToSlide(currentSlide);
-});
-
-// Previous Button
-
-prevBtn.addEventListener("click", () => {
-  if (currentSlide <= 0) {
-    goToSlide(numberOfImages - 1);
-    return;
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-white", "");
   }
-
-  currentSlide--;
-  goToSlide(currentSlide);
-});
-
-// Go To Slide
-
-function goToSlide(slideNumber) {
-  slidesContainer.style.transform =
-    "translateX(-" + slideWidth * slideNumber + "px)";
-
-  currentSlide = slideNumber;
-
-  setActiveClass();
-}
-
-// Set Active Class
-
-function setActiveClass() {
-  // Set active class for Slide Image
-
-  let currentActive = document.querySelector(".slide-image.active");
-  currentActive.classList.remove("active");
-  slideImage[currentSlide].classList.add("active");
-
-  //   set active class for navigation dots
-
-  let currentDot = document.querySelector(".single-dot.active");
-  currentDot.classList.remove("active");
-  navigationDots.children[currentSlide].classList.add("active");
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " w3-white";
 }
